aa6b33101a738aa82fdc72ce71b63d01f7e8a07a
Vasiliev Alexandr <sanek23072004@gmail.com>
2025-10-14 21:25:18 +0300

    changed jsp and servlet to work with beans, and add TestBean

--- src/main/java/ControllerServlet.java
+++ src/main/java/ControllerServlet.java
@@ -1,8 +1,11 @@
+import Beans.TestBean;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/")
@@ -10,6 +13,12 @@ public class ControllerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        session.setAttribute("name", "Tom");
+        TestBean testBean = new TestBean();
+        testBean.setName("tester");
+        testBean.setAge(0);
+        session.setAttribute("testBean", testBean);
         getServletContext().getRequestDispatcher("/main.jsp").forward(request, response);
     }
 }
--- src/main/webapp/main.jsp
+++ src/main/webapp/main.jsp
@@ -1,6 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+
 <html>
 <body>
 <h2>Hello World!</h2>
 <h1>S 1 raza?</h1>
+
+<p>Name: ${name}</p>
+
+<p>Request Name: <%= request.getAttribute("name") %></p>
+<p>Session Name: <%= session.getAttribute("name") %></p>--%>
+
+<p>Name: ${testBean.name}</p>
+<p>Age: ${testBean.age}</p>
 </body>
 </html>
